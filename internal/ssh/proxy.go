88a0abd7e2cbc93d69d7d1097b634c206578bcef
Pedro Andrade <pedro.rjandrade@gmail.com>
2023-07-17 13:55:46 -0300

    Adding the private key to the proxy config

--- internal/ssh/proxy.go
+++ internal/ssh/proxy.go
@@ -67,11 +67,11 @@ func BuildProxyConfig(socket models.Socket, AWSRegion, AWSProfile string) (*Prox
 		return nil, nil
 	}
 
-	if socket.ConnectorLocalData.UpstreamUsername == "" && socket.ConnectorLocalData.UpstreamPassword == "" &&
-		socket.ConnectorLocalData.UpstreamIdentifyFile == "" && socket.ConnectorLocalData.AWSEC2Target == "" &&
-		socket.UpstreamType != "aws-ssm" && socket.UpstreamType != "aws-ec2connect" && !socket.ConnectorLocalData.AWSEC2ConnectEnabled {
-		return nil, nil
-	}
+	// if socket.ConnectorLocalData.UpstreamUsername == "" && socket.ConnectorLocalData.UpstreamPassword == "" &&
+	// 	(len(socket.ConnectorLocalData.UpstreamIdentityPrivateKey) == 0 || socket.ConnectorLocalData.UpstreamIdentifyFile == "") && socket.ConnectorLocalData.AWSEC2Target == "" &&
+	// 	socket.UpstreamType != "aws-ssm" && socket.UpstreamType != "aws-ec2connect" && !socket.ConnectorLocalData.AWSEC2ConnectEnabled {
+	// 	return nil, nil
+	// }
 
 	if socket.UpstreamType == "aws-ssm" && socket.ConnectorLocalData.AWSECSCluster == "" && socket.ConnectorLocalData.AWSEC2Target == "" {
 		return nil, fmt.Errorf("aws_ecs_cluster or aws_ec2_target is required for aws-ssm upstream type")
@@ -100,6 +100,7 @@ func BuildProxyConfig(socket models.Socket, AWSRegion, AWSProfile string) (*Prox
 		Username:            socket.ConnectorLocalData.UpstreamUsername,
 		Password:            socket.ConnectorLocalData.UpstreamPassword,
 		IdentityFile:        socket.ConnectorLocalData.UpstreamIdentifyFile,
+		IdentityPrivateKey:  socket.ConnectorLocalData.UpstreamIdentityPrivateKey,
 		AwsEC2Target:        socket.ConnectorLocalData.AWSEC2Target,
 		AWSRegion:           AWSRegion,
 		AWSProfile:          AWSProfile,
