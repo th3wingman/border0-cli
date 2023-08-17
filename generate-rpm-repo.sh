b900dad154f7c11296abe180348e60a2979c4fcf
The Greg <greg@border0.com>
2023-08-17 19:20:50 +0200

    ME-1856: Create RedHat/Centos RPM Repository

---
+++ generate-rpm-repo.sh
@@ -0,0 +1,23 @@
+# Setup Local RPM Repository
+#!/bin/bash
+REPO_DIR="$HOME/rpm"
+REPO_FILE_PATH="$REPO_DIR/border0.repo"
+
+gpg --armor --export YOUR_EMAIL_ADDRESS > $REPO_DIR/RPM-GPG-KEY
+echo "GPG key exported to $REPO_DIR/RPM-GPG-KEY"
+
+createrepo $REPO_DIR
+echo "Repo created in $REPO_DIR"
+
+# Generate .repo file
+echo "Generating .repo file..."
+cat <<EOL | sudo tee $REPO_FILE_PATH
+[rpm]
+name=Border0 Public RPM Repository
+baseurl=http://download.border0.com/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=http://download.border0.com/rpm/RPM-GPG-KEY
+EOL
+
+echo "All done! Your repo is now hosted and ready for distribution."
