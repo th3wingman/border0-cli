e4ffd4cf84247ddc0c89ccca97e893c8bdfbea8a
Bas Toonk <bas@toonk.nl>
2023-11-20 09:19:12 +0100

    chore(ME-2309): add ssh client keepalive (#266)
    * chore(ME-2309): add ssh client keepalive
    
    * use ticker

--- internal/ssh/session/ssh_session.go
+++ internal/ssh/session/ssh_session.go
@@ -289,6 +289,7 @@ func (s *sshSession) handleChannels() error {
 
 	defer s.upstreamSshConn.Close()
 	go ssh.DiscardRequests(s.upstreamSshReqs)
+	go s.keepAlive(ctx)
 
 	for {
 		select {
@@ -322,6 +323,23 @@ func (s *sshSession) handleChannels() error {
 			}
 		case <-ctx.Done():
 			return nil
+		}
+	}
+}
+
+func (s *sshSession) keepAlive(ctx context.Context) {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			if _, _, err := s.upstreamSshConn.SendRequest("keepalive@openssh.com", true, nil); err != nil {
+				s.logger.Error("failed to send keepalive request", zap.Error(err))
+				return
+			}
 		}
 	}
 }
